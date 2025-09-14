dad41bd742cc0d6796335327b1e774f0a90d17a2
Hayden Bleasel <hello@haydenbleasel.com>
2025-09-14 15:18:06 -0700

    Remove layout config

--- apps/docs/app/(docs)/layout.tsx
+++ apps/docs/app/(docs)/layout.tsx
@@ -2,14 +2,12 @@ import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import type { ReactNode } from "react";
 import { ConditionalContainer } from "../../components/conditional-container";
 import { Navbar } from "../../components/navbar";
-import { baseOptions } from "../../lib/layout.config";
 import { source } from "../../lib/source";
 
 export default function DocsRootLayout({ children }: { children: ReactNode }) {
   return (
     <ConditionalContainer>
       <DocsLayout
-        {...baseOptions}
         nav={{
           component: <Navbar />,
         }}
